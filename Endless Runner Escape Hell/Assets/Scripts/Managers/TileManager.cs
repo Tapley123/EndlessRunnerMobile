6cdc0642c79b08b06fa00381250ed2781bc38549
Tapley123 <conortapley1@gmail.com>
2021-09-04 16:54:42 +0100

    bla

--- Endless Runner Escape Hell/Assets/Scripts/Managers/TileManager.cs
+++ Endless Runner Escape Hell/Assets/Scripts/Managers/TileManager.cs
@@ -1,18 +1,43 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TileManager : MonoBehaviour
 {
     public List<GameObject> tiles;
+    private Transform playerT;
+    private float spawnZ = 0f;
+    private float tileLength = 16.653f; // how far forward the next tile needs to be spawnd from the current one
+    [SerializeField] private int amountOfTilesOnScreen = 8;
 
     void Start()
     {
-        
+        playerT = GameObject.FindGameObjectWithTag("Player").transform;
+
+        for (int i = 0; i < amountOfTilesOnScreen; i++)
+        {
+            SpawnTile();
+        }
     }
+
 
     void Update()
     {
-        
+
+        //Everytime the player crosses a tile it spawns another one
+        if(playerT.position.z > (spawnZ - amountOfTilesOnScreen * tileLength))
+        {
+            SpawnTile();
+        }
+    }
+
+    private void SpawnTile(int prefabIndex = -1)
+    {
+        GameObject go;
+        go = Instantiate(tiles[0]) as GameObject;
+        go.transform.SetParent(this.transform);
+        go.transform.position = Vector3.forward * spawnZ;
+        spawnZ += tileLength;
     }
 }
