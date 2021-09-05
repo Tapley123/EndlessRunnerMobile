f016499e907ffbc7d49776f3b66bd1d2b49e1e20
Tapley123 <conortapley1@gmail.com>
2021-09-05 13:51:00 +0100

    optimised tile bridge working

--- Endless Runner Escape Hell/Assets/Scripts/Managers/TileManager.cs
+++ Endless Runner Escape Hell/Assets/Scripts/Managers/TileManager.cs
@@ -6,14 +6,18 @@ using UnityEngine;
 public class TileManager : MonoBehaviour
 {
     public List<GameObject> tiles;
+    private List<GameObject> activeTiles;
     private Transform playerT;
-    private float spawnZ = 0f;
+    private float spawnZ;
     private float tileLength = 16.653f; // how far forward the next tile needs to be spawnd from the current one
+    [SerializeField] private float safeZone = 25f;
     [SerializeField] private int amountOfTilesOnScreen = 8;
 
     void Start()
     {
+        activeTiles = new List<GameObject>();
         playerT = GameObject.FindGameObjectWithTag("Player").transform;
+        spawnZ = -tileLength;
 
         for (int i = 0; i < amountOfTilesOnScreen; i++)
         {
@@ -24,11 +28,11 @@ public class TileManager : MonoBehaviour
 
     void Update()
     {
-
         //Everytime the player crosses a tile it spawns another one
-        if(playerT.position.z > (spawnZ - amountOfTilesOnScreen * tileLength))
+        if(playerT.position.z - safeZone> (spawnZ - amountOfTilesOnScreen * tileLength))
         {
             SpawnTile();
+            DeleteTile();
         }
     }
 
@@ -39,5 +43,12 @@ public class TileManager : MonoBehaviour
         go.transform.SetParent(this.transform);
         go.transform.position = Vector3.forward * spawnZ;
         spawnZ += tileLength;
+        activeTiles.Add(go); //add the new tile to a list of the current tiles
+    }
+
+    private void DeleteTile()
+    {
+        Destroy(activeTiles[0]);
+        activeTiles.RemoveAt(0);
     }
 }
