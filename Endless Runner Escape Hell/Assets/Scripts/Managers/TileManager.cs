8270045e094a7cf69eccc2fa5835c8554c0d458a
Tapley123 <conortapley1@gmail.com>
2021-09-05 14:06:55 +0100

    tiles more dynamic

--- Endless Runner Escape Hell/Assets/Scripts/Managers/TileManager.cs
+++ Endless Runner Escape Hell/Assets/Scripts/Managers/TileManager.cs
@@ -8,27 +8,34 @@ public class TileManager : MonoBehaviour
     public List<GameObject> tiles;
     private List<GameObject> activeTiles;
     private Transform playerT;
-    private float spawnZ;
+
+    private float spawnZ; //at what z position is the next tile going to spawned at
     private float tileLength = 16.653f; // how far forward the next tile needs to be spawnd from the current one
-    [SerializeField] private float safeZone = 25f;
-    [SerializeField] private int amountOfTilesOnScreen = 8;
+    [SerializeField] private float safeZone = 25f; // the distance you have to be in front for a tile to be deleted behind you
+    [SerializeField] private float amountOfBlankTilesAtStart = 3f; // the amount of blank tiles (0) at the start of the game
+    [SerializeField] private int amountOfTilesOnScreen = 8; // the amount of tiles the game has on screen at any given point
+    private int lastTileIndex = 0; //the index of the last spawned tile (used to stop the same tile repeating)
 
     void Start()
     {
         activeTiles = new List<GameObject>();
         playerT = GameObject.FindGameObjectWithTag("Player").transform;
-        spawnZ = -tileLength;
+        spawnZ = -tileLength; //this will spawn a tile behind the player first
 
+        //spawns the max amount of tiles at the begining of the game
         for (int i = 0; i < amountOfTilesOnScreen; i++)
         {
-            SpawnTile();
+            if(i < amountOfBlankTilesAtStart)
+                SpawnTile(0);
+            else
+                SpawnTile(); 
         }
     }
 
 
     void Update()
     {
-        //Everytime the player crosses a tile it spawns another one
+        //Everytime the player crosses a tile it spawns a new tile in front and deletes the one at the back
         if(playerT.position.z - safeZone> (spawnZ - amountOfTilesOnScreen * tileLength))
         {
             SpawnTile();
@@ -39,7 +46,11 @@ public class TileManager : MonoBehaviour
     private void SpawnTile(int prefabIndex = -1)
     {
         GameObject go;
-        go = Instantiate(tiles[0]) as GameObject;
+        if(prefabIndex == -1)
+            go = Instantiate(tiles[RandomTileIndex()]) as GameObject;
+        else
+            go = Instantiate(tiles[prefabIndex]) as GameObject;
+        
         go.transform.SetParent(this.transform);
         go.transform.position = Vector3.forward * spawnZ;
         spawnZ += tileLength;
@@ -48,7 +59,23 @@ public class TileManager : MonoBehaviour
 
     private void DeleteTile()
     {
-        Destroy(activeTiles[0]);
-        activeTiles.RemoveAt(0);
+        Destroy(activeTiles[0]); //destroy the tile at the back
+        activeTiles.RemoveAt(0); //remove the destroyed tile from the array of active tiles
+    }
+
+    //used to pick a random tile index that isnt the last index that was used
+    private int RandomTileIndex()
+    {
+        if(tiles.Count <= 1)
+            return 0;
+
+        int randomIndex = lastTileIndex;
+        while(randomIndex == lastTileIndex)
+        {
+            randomIndex = UnityEngine.Random.Range(0, tiles.Count);
+        }
+
+        lastTileIndex = randomIndex;
+        return randomIndex;
     }
 }
