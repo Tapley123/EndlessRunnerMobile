888f4b9439c480667ef8fa4a7141df58525f962d
Tapley123 <conortapley1@gmail.com>
2021-09-06 21:14:26 +0100

    Update TileManager.cs

--- Endless Runner Escape Hell/Assets/Scripts/Managers/TileManager.cs
+++ Endless Runner Escape Hell/Assets/Scripts/Managers/TileManager.cs
@@ -10,16 +10,15 @@ public class TileManager : MonoBehaviour
     private Transform playerT;
 
     private float spawnZ; //at what z position is the next tile going to spawned at
-    private float tileLength = 16.653f; // how far forward the next tile needs to be spawnd from the current one
-    [SerializeField] private float safeZone = 10f; // 
-    private float _safeZone = 0f; // the distance you have to be in front for a tile to be deleted behind you
+    [SerializeField] private float tileLength = 16.653f; // how far forward the next tile needs to be spawnd from the current one
+    [SerializeField] private float safeZone = 10f; // amount of units you need to be in front of the tile behind you before it is deleted
     [SerializeField] private float amountOfBlankTilesAtStart = 3f; // the amount of blank tiles (0) at the start of the game
     [SerializeField] private int amountOfTilesOnScreen = 8; // the amount of tiles the game has on screen at any given point
     private int lastTileIndex = 0; //the index of the last spawned tile (used to stop the same tile repeating)
 
     void Start()
     {
-        _safeZone = tileLength + safeZone;
+        safeZone = tileLength + safeZone;
 
         activeTiles = new List<GameObject>();
         playerT = GameObject.FindGameObjectWithTag("Player").transform;
@@ -39,7 +38,7 @@ public class TileManager : MonoBehaviour
     void Update()
     {
         //Everytime the player crosses a tile it spawns a new tile in front and deletes the one at the back
-        if(playerT.position.z - _safeZone> (spawnZ - amountOfTilesOnScreen * tileLength))
+        if(playerT.position.z - safeZone> (spawnZ - amountOfTilesOnScreen * tileLength))
         {
             SpawnTile();
             DeleteTile();
