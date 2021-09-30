aaf4f1610cab2347988b96aa100994e925c2564b
Tapley123 <conortapley1@gmail.com>
2021-09-30 17:00:02 +0100

    some optimising

---
+++ Endless Runner Escape Hell/Assets/Scripts/Player/FPS.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FPS : MonoBehaviour
+{
+    private TMP_Text fpsText;
+    private float deltaTime;
+
+    private void Start()
+    {
+        fpsText = this.GetComponent<TMP_Text>();
+    }
+
+    void Update()
+    {
+        if (fpsText != null)
+        {
+            deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+            float fps = 1.0f / deltaTime;
+            fpsText.text = "FPS: " + Mathf.Ceil(fps).ToString();
+        }
+        else
+            Debug.LogError("Assign A TMP_Text To the FPS Script on the player");
+    }
+}
