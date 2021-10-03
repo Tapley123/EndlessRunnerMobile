133b74f0351b25ca1c489eb30a6029fb4be81bff
Tapley123 <conortapley1@gmail.com>
2021-10-03 16:40:55 +0100

    object pooler

---
+++ Endless Runner Escape Hell/Assets/Scripts/Managers/Game Scene/ObjectPooler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour
+{
+    public static ObjectPooler current;
+    public GameObject pooledObject;
+    public int pooledAmount;
+    public bool willGrow;
+
+    private List<GameObject> pooledObjects;
+
+    void Start()
+    {
+        current = this;
+        for (int i = 0; i < pooledAmount; i++)
+        {
+            GameObject obj = Instantiate(pooledObject);
+            obj.SetActive(false);
+            //pooledObjects
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
