5b90a8045fa57704d623b89193457465465ad475
Tapley123 <conortapley1@gmail.com>
2021-10-04 16:41:32 +0100

    Activate tiles working
    still need to deactivate them

---
+++ Endless Runner Escape Hell/Assets/Scripts/Managers/Game Scene/TileManagerOptimised.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManagerOptimised : MonoBehaviour
+{
+    private TileObjectPooler top;
+
+    public List<GameObject> tiles;
+    private List<GameObject> activeTiles;
+    private Transform playerT;
+
+    private float spawnZ; //at what z position is the next tile going to spawned at
+    [SerializeField] private float tileLength = 16.653f; // how far forward the next tile needs to be spawnd from the current one
+    [SerializeField] private float safeZone = 10f; // amount of units you need to be in front of the tile behind you before it is deleted
+    [SerializeField] private float amountOfBlankTilesAtStart = 3f; // the amount of blank tiles (0) at the start of the game
+    [SerializeField] private int amountOfTilesOnScreen = 8; // the amount of tiles the game has on screen at any given point
+    private int lastTileIndex = 0; //the index of the last spawned tile (used to stop the same tile repeating)
+
+    void Start()
+    {
+        top = this.GetComponent<TileObjectPooler>();
+
+        safeZone = tileLength + safeZone;
+
+        activeTiles = new List<GameObject>();
+        playerT = GameObject.FindGameObjectWithTag("Player").transform;
+        spawnZ = -tileLength; //this will spawn a tile behind the player first
+
+        SpawnStartTiles();
+    }
+
+
+    void Update()
+    {
+        //Everytime the player crosses a tile it spawns a new tile in front and deletes the one at the back
+        if (playerT.position.z - safeZone > (spawnZ - amountOfTilesOnScreen * tileLength))
+        {
+            ActivateTile();
+            //DeactivateTile();
+        }
+    }
+
+    private void SpawnStartTiles()
+    {
+        for (int i = 0; i < top.blankTiles.Count; i++)
+        {
+            Debug.Log(i);
+            top.blankTiles[i].SetActive(true);
+            top.blankTiles[i].transform.position = Vector3.forward * spawnZ;
+            spawnZ += tileLength;
+        }
+    }
+
+    private void ActivateTile()
+    {
+        GameObject go = top.GetRandomTile();
+        go.SetActive(true);
+        go.transform.position = Vector3.forward * spawnZ;
+        spawnZ += tileLength;
+    }
+
+    private void DeactivateTile()
+    {
+        Destroy(activeTiles[0]); //destroy the tile at the back
+        activeTiles.RemoveAt(0); //remove the destroyed tile from the array of active tiles
+    }
+
+    //used to pick a random tile index that isnt the last index that was used
+    private int RandomTileIndex()
+    {
+        if (tiles.Count <= 1)
+            return 0;
+
+        int randomIndex = lastTileIndex;
+        while (randomIndex == lastTileIndex)
+        {
+            randomIndex = UnityEngine.Random.Range(0, tiles.Count);
+        }
+
+        lastTileIndex = randomIndex;
+        return randomIndex;
+    }
+}
---
+++ Endless Runner Escape Hell/Assets/Scripts/Managers/Game Scene/TileObjectPooler.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileObjectPooler : MonoBehaviour
+{
+    public static TileObjectPooler current;
+    public List<GameObject> tilesToPool;
+    public int amountOfEachTileToSpawn = 3;
+    private int pooledAmount;
+
+    [SerializeField] private List<GameObject> pooledObjects;
+    public List<GameObject> blankTiles;
+
+    void Awake()
+    {
+        current = this;
+        SpawnStartTiles();
+        GetBlankTiles();
+    }
+
+    void SpawnStartTiles()
+    {
+        pooledObjects = new List<GameObject>();
+        pooledAmount = tilesToPool.Count * amountOfEachTileToSpawn; //calculates the amount of tiles to spawn
+        //Debug.Log("Pooled Amount = " + pooledAmount);
+
+        //executes once per each tile that needs to be spawned into the pool
+        for (int i = 0; i < pooledAmount; i++)
+        {
+            //adds (amountOfEachTile) to the pool 
+            for (int x = 0; x < amountOfEachTileToSpawn; x++)
+            {
+                if (i < tilesToPool.Count)
+                {
+                    GameObject obj = Instantiate(tilesToPool[i]);
+                    obj.SetActive(false);
+                    obj.transform.parent = this.transform;
+                    pooledObjects.Add(obj);
+                }
+            }
+        }
+    }
+
+    private void GetBlankTiles()
+    {
+        blankTiles = new List<GameObject>();
+
+        foreach(GameObject go in pooledObjects)
+        {
+            if(go.name == tilesToPool[0].gameObject.name + "(Clone)")
+            {
+                blankTiles.Add(go);
+            }
+        }
+    }
+
+    public GameObject GetRandomTile()
+    {
+        int randomNum = Random.Range(0, pooledObjects.Count);
+        while (pooledObjects[randomNum].activeInHierarchy)
+        {
+            randomNum = Random.Range(0, pooledObjects.Count);
+        }
+
+        //found a random tile that is not currently being used
+        if (!pooledObjects[randomNum].activeInHierarchy)
+        {
+            return pooledObjects[randomNum]; //return the random tile that is not currently being used
+        }
+
+        return null;
+    }
+}
